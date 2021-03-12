6d3e01cbefff8b5be90d26f8de0355600917f95c
scratchingmycranium <41268767+scratchingmycranium@users.noreply.github.com>
2021-03-12 14:07:20 -0500

    fix

--- routes/test.js
+++ routes/test.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.post('/test', function(req, res, next) {
+router.post('/', function(req, res, next) {
+  console.log(req)
   res.send('respond with a resource');
 });
 
